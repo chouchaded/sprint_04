50fd16b4508993b9a4b23f52fb1dd1afa09188d7
YanSub <chouchade@gmail.com>
2022-02-15 15:58:17 +0400

    Level 02 (debut)

---
+++ day_01/starter_day01_L02/script.js
@@ -0,0 +1,11 @@
+let phrase1="The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+console.log(phrase1);
+
+let phrase2="\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\""
+console.log(phrase2);
+
+let num=10;
+
+console.log("num :",num);
+console.log("type de variable num et str:",typeof(num));
+console.log("test variable num et 10",num === 10);
