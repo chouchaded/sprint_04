bc4ae49c8d527d728c1681164153aefbcaea2822
YanSub <chouchade@gmail.com>
2022-02-17 11:33:23 +0400

    Random level 04

---
+++ day_01/starter_day01_random_L04/script.js
@@ -0,0 +1,20 @@
+let a = 3;
+let b = 4;
+
+if (a>b) {
+  console.log("a est superieur à b")} else {
+    console.log("va te rouler");
+}
+
+if (a<b) {
+  console.log("a est superieur à b")} else {
+    console.log("va te rouler");
+}
+
+
+const aNumber = Number(window.prompt("Veuillez saisir un nombre.", ""));
+if (aNumber % 2 == 0) {
+    console.log("Ce nombre est pair")}
+else {
+    console.log("Ce nombre est impair")
+}
