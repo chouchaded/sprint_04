59ff3118b9aba4285bb1fca97b49db756855bd9d
YanSub <chouchade@gmail.com>
2022-02-18 09:25:27 +0400

    condition level 02

---
+++ day_01/starter_day01_conditions_L02/script1.js
@@ -0,0 +1,12 @@
+const mois = String(window.prompt("Veuillez saisir un mois de l'année:"));
+if (mois === "septembre" || mois === "octobre" || mois === "novembre") {
+    console.log("C'est l'automne")
+} else if
+(mois === "decembre" || mois === "janvier" || mois === "fevrier") {
+console.log("C'est l'hiver")
+} else if (mois === "mars" || mois === "avril" || mois === "mai") {
+    console.log("C'est le printemps")
+} else if
+    (mois === "juin" || mois === "juillet" || mois === "aout") {
+    console.log("C'est l'été")
+}
